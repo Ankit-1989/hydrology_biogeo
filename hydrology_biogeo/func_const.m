42e8b7b0a835e5f275eff3035f5f60757a14aa2e
Ankit <ankit.pramanik@outlook.com>
2022-01-16 13:54:27 +0530

    Added constant function file

---
+++ hydrology_biogeo/func_const.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Constants
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [C] = func_const()
+
+C.conc_bc = 4.56e-4;    % Concentration at the start of the channel
+C.ca_out = 4.56e-4; %10;    % concetration in cavity % uniform
+C.dt = 1;
+C.cn = 0;
+
